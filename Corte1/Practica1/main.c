f19ab7b4c84a06ab6ba034b9ef183605cb41759e
nicoproo777 <cortesnicolas097@gmail.com>
2025-09-05 12:59:22 -0500

    Add files via upload

---
+++ Corte1/Practica1/main.c
@@ -0,0 +1,15 @@
+#include <iostream>
+
+/*
+ run this program using the console pauser or add your own getch, system("pause") or input loop
+ hola estoy practicando programacion
+
+*/
+
+//quiero aprender a programar en c
+
+int main(int argc, char** argv) {
+	
+	printf("hola mundo");
+	return 0;
+}
