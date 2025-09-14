304e85774ce891dc0952f54d37998f13c7742f9d
nicoproo777 <cortesnicolas097@gmail.com>
2025-09-14 13:53:11 -0500

    Update main.c

--- Corte1C/Practica4/main.c
+++ Corte1C/Practica4/main.c
@@ -9,19 +9,20 @@ int main(int argc, char *argv[]) {
 	a=8;
 	b=25;
 	r=a+b;
-	printf ("El resultado de sumar %d y %d es: %d\n",a,b, r);
+	printf ("The result of adding %d and %d is: %d\n",a,b, r);
 	
 	if(r == 40){
-		printf("El resultado %d si es igual = 40");
+		printf("The result %d if it is equal to = 40");
 	}
 	else{
 		if(r < 40){
-			printf("El resultado %d si es < 40",r);	
+			printf("The result %d its equal to < 40",r);	
 		}
 		else{
-			printf("El resultado si es > 40",r);
+			printf("The result it is > 40",r);
 		}
 	}
 	
 	return 0;
 }
+
