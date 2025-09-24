6506ff06795d292adfd86c28242f6b2d598f7223
nicoproo777 <cortesnicolas097@gmail.com>
2025-09-23 19:28:40 -0500

    Add files via upload

---
+++ Corte1C++/Practica10_funciones_/main9.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int sumar(int a, int b){
+	
+	int r=a+b;
+	return r;
+	
+}
+
+int restar(int h, int x){
+	
+	int d=h-x;
+	return d;
+	
+}
+
+int multiplicar(int p, int q){
+	
+	int e=p*q;
+	return e;
+	
+}	
+int main(){
+
+ int h=sumar(7,31);
+ cout<<h<<endl;
+ 
+ int z=restar(9,7);
+ cout<<z<<endl;
+ 
+ int l=multiplicar(8,8);
+ cout<<l<<endl;
+ 
+}
