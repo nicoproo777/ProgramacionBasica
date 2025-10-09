c6ed86f57b745a1b52456dc907fe59adb8de9e63
nicoproo777 <cortesnicolas097@gmail.com>
2025-10-09 13:19:47 -0500

    Add files via upload

---
+++ Corte1C/Arrays3/Arrays3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	  int myNumbers[4];
+
+  myNumbers[0] = 25;
+  myNumbers[1] = 50;
+  myNumbers[2] = 75;
+  myNumbers[3] = 100;
+
+  printf("%d\n", myNumbers[0]);
+	return 0;
+}
