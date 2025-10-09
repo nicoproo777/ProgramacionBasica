5837c54e75ae22fcdaf23e8bfb96302538325bc9
nicoproo777 <cortesnicolas097@gmail.com>
2025-10-09 13:20:33 -0500

    Add files via upload

---
+++ Corte1C/MyArrays/MyArrays.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main() {  
+
+int myArray[] = {25, 50, 75, 3.15, 5.99};
+  int i;
+  
+  for (i = 0; i < 5; i++) {
+    printf("%d\n", myArray[i]);
+  }
+ 
+	return 0;
+}
