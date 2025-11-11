72205ad28b9e15a0f7f4c04cfa4dc74492007243
nicoproo777 <cortesnicolas097@gmail.com>
2025-11-11 18:37:56 -0500

    Add files via upload

---
+++ Corte2/C/Actividad_06-11-2025/Resta/main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+// Funcion para ingresar un valor
+int enterValue() {
+    int value;
+    printf("Enter value: ");
+    scanf("%d", &value);
+    return value;
+    }
+
+// Funcion para restar dos numeros
+int add(int a, int b) {
+    int r = a - b;
+    return r;
+    }
+
+// Funcion para mostrar el resultado
+void showResult(int r) {
+    printf("Result = %d\n", r);
+    }
+    
+int main() {
+    int operand1, operand2;
+    int result;
+
+    operand1 = enterValue();
+    operand2 = enterValue();
+
+    result = add(operand1, operand2);
+
+    showResult(result);
+
+    return 0;
+}
+
